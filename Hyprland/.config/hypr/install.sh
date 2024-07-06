e175c0edf8abc69affed8ac997411ec6adfb0d74
Alan-J-Bibins <alanjbibins@gmail.com>
2024-07-06 22:09:38 +0530

    Tweaks to avoid using nvidia, and added updated post command for waypaper

--- Hyprland/.config/hypr/install.sh
+++ Hyprland/.config/hypr/install.sh
@@ -1,2 +1,4 @@
 
-sudo pacman -S hyprland hyprlock cpio xorg-xhost hyprwayland-scanner xdg-desktop-portal xdg-desktop-portal-hyprland wl-clipboard nwg-look qt5-wayland qt6-wayland qt5ct qt6ct kvantum swaync kvantum-theme-materia materia-kde materia-gtk-theme kvantum-qt5 cliphist
+sudo pacman -S hyprland hyprlock cpio xorg-xhost hyprwayland-scanner xdg-desktop-portal xdg-desktop-portal-hyprland wl-clipboard nwg-look qt5-wayland qt6-wayland qt5ct qt6ct kvantum swaync kvantum-theme-materia materia-kde materia-gtk-theme kvantum-qt5 cliphist swww unzip ripgrep
+
+yay -S waypaper bibata-cursor-theme oh-my-posh
