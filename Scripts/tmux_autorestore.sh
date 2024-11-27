637724ec59ae9c359c8fab521b8ff9eb694ab167
Alan-J-Bibins <alanjbibins@gmail.com>
2024-11-27 22:05:17 +0530

    lesgo

--- Scripts/tmux_autorestore.sh
+++ Scripts/tmux_autorestore.sh
@@ -1,6 +1,8 @@
-echo "Creating temporary session..."
+#!/bin/bash
+
+echo "Starting tmux session restoration at $(date)"
 tmux new-session -d -s _hidden
-sleep 1
+sleep 3
 
 echo "Restoring Sessions..."
 tmux run-shell ~/.tmux/plugins/tmux-resurrect/scripts/restore.sh
