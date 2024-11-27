8cce235ad30d945a7f3aaa7d90a5dc15557a00cf
Alan-J-Bibins <alanjbibins@gmail.com>
2024-11-27 21:38:31 +0530

    tweaks to tmux_autorestore

--- Scripts/tmux_autorestore.sh
+++ Scripts/tmux_autorestore.sh
@@ -1,3 +1,11 @@
+echo "Creating temporary session..."
 tmux new-session -d -s _hidden
+sleep 1
+
+echo "Restoring Sessions..."
 tmux run-shell ~/.tmux/plugins/tmux-resurrect/scripts/restore.sh
+
+echo "Killing temporary session..."
+sleep 1
 tmux kill-session -t _hidden
+echo "DONE!!"
