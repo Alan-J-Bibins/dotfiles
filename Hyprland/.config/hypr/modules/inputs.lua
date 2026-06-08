799c94c74bd66e790666129c836f1f1657ddf74b
Alan-J-Bibins <alanjbibins@gmail.com>
2026-06-08 12:34:07 +0530

    feat: Gesture to deactivate current special workspace

--- Hyprland/.config/hypr/modules/inputs.lua
+++ Hyprland/.config/hypr/modules/inputs.lua
@@ -1,3 +1,11 @@
+function CloseSpecialWorkspace()
+    local current = hl.get_active_special_workspace()
+    if current then
+        local name = current.config_name:gsub("^special:", "")
+        hl.dispatch(hl.dsp.workspace.toggle_special(name))
+    end
+end
+
 hl.config({
     input = {
         kb_layout = "us",
@@ -22,4 +30,12 @@ hl.gesture({
    fingers = 3,
    direction = "horizontal",
    action = "workspace"
+})
+
+hl.gesture({
+   fingers = 3,
+   direction = "down",
+   action = function ()
+       CloseSpecialWorkspace()
+   end
 })
